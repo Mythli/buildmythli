c566a748821d78f138a031bcfc5292bf70c12e0f
Mythli <tobiasdev@live.de>
2012-08-21 23:08:50 +0200

    merged

--- buildmythli.sh
+++ buildmythli.sh
@@ -1,19 +1,84 @@
 #!/bin/bash
-ScriptDir=`dirname $0`
-source "$ScriptDir/libbuildmythli.sh"
 
+BUILDMYTHLI_CHECKOUT=0
+BUILDMYTHLI_HELP=1
+BUILDMYTHLI_VERSION=2
 
-rm -Rf "/home/tobias/Develop/projects/buildmythli/test"
-InitRepository "https://github.com/Mythli/SqlDatabase.git" "/home/tobias/Develop/projects/buildmythli/test/git"
-#InitRepository "http://nginx.org/download/nginx-1.2.3.tar.gz" "/home/tobias/Develop/projects/buildmythli/test/archive"
-#InitRepository "http://cwowcms.googlecode.com/svn" "/home/tobias/Develop/projects/buildmythli/test/svn"
+function PrintHelp
+{
+	echo "Usage: buildmythli [OPTION]... [URL]... [DIRECTORY]..."
+	echo ""
+	echo "	-h --help 		display this help and exit"
+	echo "	-v --version 		output version information and exit"
+	echo "	-c --checkout 		checkout a repository and generate the buildmythli script"
+	echo "	   --checkout-type 	forces buildmythli to use this vcs rather than determining it"
+	echo "	-u --url 		set the url for repository checkouts"
+	echo "	-d --directory 		set the director into which the repository is checked out"
+	echo ""
+	echo "Report buildmythli bugs to build@mythli.net"
+	exit 0
+}
 
-declare -A buildmythliArgs=( 
-	["builddit"]="cow"
-	["aa"]="test"
-)
+function PrintVersion
+{
+	echo "buildmythli version 0.1"
+	exit 0
+}
 
-#InitRepository "svn://svn.lighttpd.net/xcache" "/home/tobias/Develop/projects/buildmythli/test/xcache"
-#InitRepository "https://github.com/php/php-src.git" "/home/tobias/Develop/projects/buildmythli/test/php" "PHP-5.4.6"
+function CheckoutRepository
+{
+	echo ""
+}
 
-#GenMakeFiles "/home/tobias/Develop/projects/buildmythli/test/archive" "$(declare -p buildmythliArgs)"
+# Execute getopt
+GetOptEscapeHelper=`getopt -o "hvcud" -l "help,version,checkout,url,dir:" \
+      -n "buildmythli" -- "$@"`
+eval set -- "$GetOptEscapeHelper"
+
+Dir=""
+Url=""
+Mode=MODE_CHECKOUT
+
+while true;
+do
+	argName=$1
+	argValue=$2
+	
+	case "$argName" in
+		-h|--help)
+			Mode=MODE_HELP
+			shift
+		;;
+		-v|--version)
+			Mode=MODE_VERSION
+			shift
+		;;
+		--checkout-type)
+			
+		;;
+		-u|--url)
+			Url=$argValue
+			shift 2
+		;;
+		-d|--directory)
+			Dir=$argValue
+			shift 2
+		;;
+		--)
+		shift
+		break;;
+	esac
+done
+
+case $Mode in
+	BUILDMYTHLI_HELP)
+		PrintHelp
+	;;
+	BUILDMYTHLI_VERSION)
+		PrintVersion
+	;;
+	BUILDMYTHLI_CHECKOUT)
+		
+		CheckoutRepository "$Url" "$Dir"
+	;;
+esac
