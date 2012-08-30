2370e2724f396acb9d5ce393f5f5859988c7e54a
Mythli <tobiasdev@live.de>
2012-08-30 22:50:57 +0200

    refactored checkout to init, fixed some typos and implemented test mode

--- buildmythli.sh
+++ buildmythli.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e
+BaseDir=$(dirname $0)
+source "$BaseDir/libbuildmythli.sh"
 
-BUILDMYTHLI_CHECKOUT=0
+BUILDMYTHLI_INIT=0
 BUILDMYTHLI_HELP=1
 BUILDMYTHLI_VERSION=2
+BUILDMYTHLI_TEST=3
 
 function PrintHelp
 {
@@ -10,12 +14,12 @@ function PrintHelp
 	echo ""
 	echo "	-h --help 		display this help and exit"
 	echo "	-v --version 		output version information and exit"
-	echo "	-c --checkout 		checkout a repository and generate the buildmythli script"
-	echo "	   --checkout-type 	forces buildmythli to use this vcs rather than determining it"
-	echo "	-u --url 		set the url for repository checkouts"
-	echo "	-d --directory 		set the director into which the repository is checked out"
+	echo "	-i --init 		init a repository and generate the buildmythli script"
+	echo "	   --init-type 		forces buildmythli to use this vcs rather than determining it"
+	echo "	-u --url 		set the url for repository init"
+	echo "	-d --dir --directory 		set the director into which the repository is checked out"
 	echo ""
-	echo "Report buildmythli bugs to build@mythli.net"
+	echo "Report buildmythli bugs to buildmythli@projects.mythli.net"
 	exit 0
 }
 
@@ -25,19 +29,54 @@ function PrintVersion
 	exit 0
 }
 
-function CheckoutRepository
+function BuildMythliScript
 {
-	echo ""
+}
+
+function InitRepository
+{
+	local url=$1
+	local dir=$2
+	local type=$3
+	
+	echo 'Creating folder Structure...'
+	CreateFolderStructure "$dir"
+	echo 'Downloading source...'
+	Checkout "$url" "$dir" $type
+	echo 'Writing buildmythli script to file...'
+	BuildMythliScript "$url" "$dir" $type
+}
+
+function ExecuteMode
+{
+	case $Mode in
+		BUILDMYTHLI_HELP)
+			PrintHelp
+		;;
+		BUILDMYTHLI_VERSION)
+			PrintVersion
+		;;
+		BUILDMYTHLI_INIT)
+			InitRepository "$Url" "$Dir"
+		;;
+		BUILDMYTHLI_TEST)
+			rm -Rf "$BaseDir/test"
+			mkdir "$BaseDir/test"
+			
+			echo 'Git test...'
+			$BaseDir/buildmythli.sh --init --url=https://github.com/Mythli/buildmythli.git --dir=/home/tobias/Develop/projects/buildmythli/test/git
+		;;
+	esac
 }
 
 # Execute getopt
-GetOptEscapeHelper=`getopt -o "hvcud" -l "help,version,checkout,url,dir:" \
+GetOptEscapeHelper=`getopt -o "hviu:d:" -l "help,version,init,init-type:,url:,dir:,test" \
       -n "buildmythli" -- "$@"`
 eval set -- "$GetOptEscapeHelper"
 
 Dir=""
 Url=""
-Mode=MODE_CHECKOUT
+Mode=BUILDMYTHLI_INIT
 
 while true;
 do
@@ -46,21 +85,29 @@ do
 	
 	case "$argName" in
 		-h|--help)
-			Mode=MODE_HELP
+			Mode=BUILDMYTHLI_HELP
 			shift
 		;;
 		-v|--version)
-			Mode=MODE_VERSION
+			Mode=BUILDMYTHLI_VERSION
 			shift
 		;;
-		--checkout-type)
-			
+		-i|--init)
+			Mode=BUILDMYTHLI_INIT
+			shift
+		;;
+		--test)
+			Mode=BUILDMYTHLI_TEST
+			shift
+		;;
+		--init-type)
+			shift
 		;;
 		-u|--url)
 			Url=$argValue
 			shift 2
 		;;
-		-d|--directory)
+		-d|--dir|--directory)
 			Dir=$argValue
 			shift 2
 		;;
@@ -70,15 +117,4 @@ do
 	esac
 done
 
-case $Mode in
-	BUILDMYTHLI_HELP)
-		PrintHelp
-	;;
-	BUILDMYTHLI_VERSION)
-		PrintVersion
-	;;
-	BUILDMYTHLI_CHECKOUT)
-		
-		CheckoutRepository "$Url" "$Dir"
-	;;
-esac
+ExecuteMode
